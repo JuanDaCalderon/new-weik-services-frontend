fe2229487f093115dddd49175c70adf895bae498
juan david calderon jimenez <j.calderonj@uniandes.edu.co>
2025-02-02 17:14:29 -0500

    add devOptions enabled

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,9 @@ export default defineConfig({
     VitePWA({
       registerType: 'autoUpdate',
       includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
+      devOptions: {
+        enabled: true
+      },
       manifest: {
         name: 'Weik Services',
         short_name: 'Weik',
